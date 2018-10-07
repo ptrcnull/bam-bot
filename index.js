cb4d587d60affb48c099f4c0c37b5281c84efd78
Bjornskjald <github@bjorn.ml>
2018-10-07 23:02:12 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('Discord.js')
+const client = new Discord.Client()
+const config = require('./config.json')
+
+client.login(config.token).then(() => {
+  client.on('message', message => {
+    if (message.cleanContent.startsWith('!bam')) {
+      message.channel.send({
+        files: [
+          {
+            attachment: 'https://cdn.discordapp.com/attachments/456818557519659011/496761562850131978/unknown.png',
+            name: 'bam.png'
+          }
+        ]
+      })
+    }
+  })
+})
